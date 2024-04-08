6eb89b8d96bbfc5e1fb099281249382c67aac6c9
Hugo Guarido <hguarido@gmail.com>
2024-04-08 11:47:45 +0200

    Nueva clase ProductManager y métodos de clase MaquinaVending

--- MaquinaVending/MaquinaVending/MaquinaVending.cs
+++ MaquinaVending/MaquinaVending/MaquinaVending.cs
@@ -8,6 +8,41 @@ namespace MaquinaVending
 {
     internal class MaquinaVending
     {
+        List<Producto> listaProductos;
 
+        public MaquinaVending()
+        {
+            listaProductos = new List<Producto>();
+        }
+
+        // Método encargado de proceso de compra de los productos 
+        public void ComprarProducto()
+        {
+
+        }
+
+        // Método que muestra la información completa de los productos
+        public void MostrarInfoProductos()
+        {
+
+        }
+
+        // Método que permite al Admin reponer productos existentes o añadir nuevos
+        public void CargaIndividual()
+        {
+
+        }
+
+        // Método que permite al Admin reponer completamente las unidades de los productos existentes
+        public void CargaCompleta()
+        {
+
+        }
+
+        // Método que guarda los datos y cierra el programa
+        public void SalirGuardar()
+        {
+
+        }
     }
 }
---
+++ MaquinaVending/MaquinaVending/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaVending
+{
+    
+    internal class ProductManager
+    {
+        public List<Producto> listaProductos;
+
+        public ProductManager(List<Producto> listaP)
+        {
+            listaProductos = listaP;
+        }
+    }
+}
