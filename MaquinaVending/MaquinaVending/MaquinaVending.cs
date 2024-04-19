44e1bfa6009b96fbd7733fbcdc42b18076f14109
Hugo Guarido <hguarido@gmail.com>
2024-04-19 13:30:27 +0200

    Cambios a clase MaquinaVending

--- MaquinaVending/MaquinaVending/MaquinaVending.cs
+++ MaquinaVending/MaquinaVending/MaquinaVending.cs
@@ -29,35 +29,49 @@ namespace MaquinaVending
             int opcion = 0;
             double precioTotal = 0;
 
-            Console.WriteLine(" --- PRODUCTOS DISPONIBLES ---");
-
-            foreach(Producto p in listaProductos)
+            if (listaProductos.Count == 0)
             {
-                Console.WriteLine(p.MostrarInfo());
+                Console.WriteLine("La máquina no contiene productos.");
             }
-
-            do
+            else
             {
-                Producto productoElegido = productManager.ElegirProducto();
-                carrito.Add(productoElegido);
-                precioTotal =+ productoElegido.PrecioUnidad;
-                productoElegido.Unidades--;
-                Console.WriteLine($"El producto {productoElegido.Nombre} ha sido añadido al carrito!");
+                Console.WriteLine(" --- PRODUCTOS DISPONIBLES ---");
 
-                Console.Write("Desea comprar otro producto? (1.Sí / 2.No): ");
-                opcion = int.Parse(Console.ReadLine());
+                foreach (Producto p in listaProductos)
+                {
+                    Console.WriteLine(p.MostrarInfo());
+                }
 
-            } while (opcion == 1);
+                do
+                {
+                    Producto productoElegido = productManager.ElegirProducto();
 
-            Console.WriteLine("Tu carrito incluye:");
+                    if (productoElegido != null)
+                    {
+                        carrito.Add(productoElegido);
+                        precioTotal = +productoElegido.PrecioUnidad;
+                        productoElegido.UnidadesDisponibles--;
+                        Console.WriteLine($"El producto {productoElegido.Nombre} ha sido añadido al carrito!");
 
-            foreach(Producto p in carrito)
-            {
-                Console.WriteLine($"{p.Nombre}");
+                        Console.Write("Desea comprar otro producto? (1.Sí / 2.No): ");
+                        opcion = int.Parse(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.WriteLine("El producto con el ID introducido no esta disponible.");
+                    }
+                } while (opcion == 1);
+
+                Console.WriteLine("Tu carrito incluye:");
+
+                foreach (Producto p in carrito)
+                {
+                    Console.WriteLine($"{p.Nombre}");
+                }
+
+                Console.WriteLine($"El precio total a pagar es de {precioTotal}€.");
+                Pagar(precioTotal);
             }
-
-            Console.WriteLine($"El precio total a pagar es de {precioTotal}€.");
-            Pagar(precioTotal);
         }
 
         // Método para pagar carrito
@@ -101,18 +115,33 @@ namespace MaquinaVending
         public void CargaIndividual()
         {
             int numeroSlots = 12;
-
             bool accesoAdmin = CheckAdmin();
 
             if (accesoAdmin)
             {
-                int opcion;
+                Console.WriteLine("Desea reponer o añadir productos?");
+                Console.WriteLine("1: Reponer\n2: Añadir");
+                int opcion = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("");
-            }
-            else
-            {
-
+                switch(opcion)
+                {
+                    case 1:
+                        ReponerProducto();
+                        break;
+                    case 2:
+                        if (listaProductos.Count < numeroSlots)
+                        {
+                            AddNewProduct();
+                        }
+                        else
+                        {
+                            Console.WriteLine("La capacidad de la máquina esta llena. No se pueden añadir productos.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+                }
             }
         }
 
@@ -122,10 +151,6 @@ namespace MaquinaVending
             bool accesoAdmin = CheckAdmin();
 
             if (accesoAdmin)
-            {
-
-            }
-            else
             {
 
             }
@@ -149,6 +174,10 @@ namespace MaquinaVending
             {
                 Console.WriteLine("Clave secreta correcta. Bienvenido Admin.");
                 check = true;
+            }
+            else if (clave != ClaveSecreta)
+            {
+                Console.WriteLine("Clave secreta incorrecta.");
             }
             return check;
         }
